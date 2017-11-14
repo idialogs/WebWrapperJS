cb588f1a27d949d549d3879995220a698d1b7bf0
Evan Masseau <evan@idialogs.com>
2017-11-14 12:47:20 -0500

    Bring in existing JS from ios app.

--- wrapper.js
+++ wrapper.js
@@ -1,7 +1,171 @@
-function WebWrapper(opts) {
-    this.opts = opts;
+/**
+ * To launch, inject this script file from native code at the top of the document
+ * Format string like this, where strWrapperScript is the contents of this file
+ * and strHandoff is the json encoded handoff parameters detailed below
+ * Always be careful to put quotes around your string arguments! *
+ *
+ *    ";(" + strWrapperScript + ")('" + strHandoff + "');"
+ *
+ * To execute wrapper methods, inject js strings like this.
+ *    "IdaMobileAppBrowsing.methodName('string')"
+ *
+ * @param strHandoff Should contain the following values
+ *  {
+ *      appName: Name of the app, up to developer to name it
+ *      css_class: String class name to add to html tag for this app - can be used for CSS
+ *      oauthToken: User's oauth token
+ *      debugAjax: Bool value whether to post every ajax URL to the native app, recommend FALSE in production builds
+ *      isWrapperApp: Bool whether app is running as true webview wrapper or native app with few webviews
+ *      version: String of what version/build app is running
+ *      server: String server name app is connected to
+ *  }
+ */
+function wrapper(strHandoff) {
 
-    window.IdaMobileAppBrowsing = {
-        token: opts.oauthToken
-    };
+    //Initialize web wrapper object, communicates to web JS that we are in mobile browsing mode
+    window.IdaMobileAppBrowsing = new WebWrapper(JSON.parse(strHandoff));
+
+    /**
+     * Delayed initialization for after DOM ready
+     * Equivalent to documentReady from jQuery
+     */
+    document.addEventListener("DOMContentLoaded", function (e) {
+        window.IdaMobileAppBrowsing.launchApp();
+    });
+
+    /**
+     * WebWrapper module contains all
+     * @param opts
+     * @returns {{postToNativeApp: function}}
+     * @constructor
+     */
+    function WebWrapper(opts) {
+
+        //Launch ajax debugging if requested
+        if (opts.debugAjax) {
+            setTimeout(debugAjax, 5);
+        }
+
+        return {
+            /**
+             * For debugging, make opts from launch public
+             */
+            launchOpts: opts,
+
+            /**
+             * Communicate oauth token to web JS
+             */
+            token: opts.oauthToken,
+
+            /**
+             * Set/update oauth token
+             * @param token
+             * @returns {*}
+             */
+            setToken: function (token) {
+                return this.token = token
+            },
+
+            /**
+             * Bool test if this is the iOS webwrapper
+             */
+            isIOS: !!window.webkit && !!window.webkit.messageHandlers[opts.appName],
+
+            /**
+             * Bool test if this is android webwrapper
+             * TODO warrington -- add test for android
+             */
+            isAndroid: false,
+
+            /**
+             * Method to post string message to native app
+             * @param str
+             */
+            postToNativeApp: function (str) {
+                if (this.isIOS) {
+                    window.webkit.messageHandlers[opts.appName].postMessage(str);
+                } else if (this.isAndroid) {
+                    //TODO Warrington - post str to android app
+                }
+            },
+
+            /**
+             * Perform native-initiated navigation via ajax call for speed
+             */
+            ajaxNavigation: function (url) {
+                var $page = $('.page');
+
+                $page.hide();
+
+                $.publish(
+                    'ajax/load',
+                    {
+                        url: url,
+                        callback: function () {
+                            $page.show();
+                        }
+                    }
+                );
+            },
+
+            /**
+             * Opens the push navigation menu (hamburger menu)
+             */
+            showNavigation: function () {
+                $.publish('mlpushmenu/toggle');
+            },
+
+            /**
+             * On document ready logic
+             */
+            launchApp: function () {
+                var self = this;
+
+                if (opts.css_class) {
+                    $(document.documentElement).addClass(opts.css_class);
+                }
+
+                // Inform native app of document ready and whether we are logged in
+                this.postToNativeApp({docReady: true});
+
+                //Now that publish is available, notify web JS that app browsing is active
+                $.publish('idaMobileApp/load', opts.appName);
+
+                //Forgot password screen: notify user in native app to check their email after resetting PW
+                $(document.body).on(
+                    'click',
+                    '#backSent',
+                    function (e) {
+                        e.preventDefault();
+
+                        self.postToNativeApp(
+                            {
+                                nativeNavigation: "back",
+                                showAlert: "Check your email..."
+                            }
+                        );
+                    }
+                );
+            }
+        }
+    }
+
+    /**
+     * Utility to communicate the URL of all jQuery ajax calls back to native app
+     * Toggle on with "debugAjax" option
+     */
+    function debugAjax() {
+        if (!window.$) {
+            setTimeout(debugAjax, 5);
+            return;
+        }
+
+        var ajax = $.ajax;
+
+        $.ajax = function () {
+            IdaMobileAppBrowsing.postToNativeApp("Ajax url: " +
+                                                 arguments[0].url)
+            return ajax.apply($, arguments);
+        }
+    }
 }
